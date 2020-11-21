b1a64605a0a8de064de62af254e6b3c5d2cf24cb
Chopin <m.chopin@student.isartdigital.com>
2020-11-21 20:37:48 +0100

    Create Function to get row or column

--- Assets/Scripts/Rubikscube.cs
+++ Assets/Scripts/Rubikscube.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public struct ResultRayCast
+{
+    public Vector3 positionMouse;
+    public Vector3 normalFace;
+    public int indexFace;
+    public bool isDefinited;
+}
 
 public class Rubikscube : MonoBehaviour
 {
@@ -13,6 +22,23 @@ public class Rubikscube : MonoBehaviour
     
     private List<GameObject> m_cubes;
 
+    private ResultRayCast m_resultRayCast;
+    
+    
+    [SerializeField] private GameObject touchTemp;
+    [SerializeField] private Material newMat;
+    
+    
+    public int sizeRubiksCube
+    {
+        get
+        {
+            return m_width * m_heigth;
+        }
+    }
+
+    
+    
     private void Awake()
     {
         m_cubes = new List<GameObject>();
@@ -33,12 +59,105 @@ public class Rubikscube : MonoBehaviour
                 }
             }
         }
+
+        m_resultRayCast.isDefinited = false;
     }
     
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                GameObject parent = hit.collider.gameObject.transform.parent.gameObject;
+                if (m_resultRayCast.isDefinited == false)
+                {
+                    m_resultRayCast.normalFace = hit.normal;
+                    m_resultRayCast.indexFace = m_cubes.IndexOf(parent);
+                    m_resultRayCast.positionMouse = hit.point;
+                    m_resultRayCast.isDefinited = true;
+                
+                    touchTemp.transform.position = m_resultRayCast.positionMouse;
+                
+                    List<GameObject> row = GetRow(m_resultRayCast.indexFace, hit.normal);
+                    for (int i = 0; i < row.Count; i++)
+                    {
+                        for (int j = 0; j < 6; j++)
+                        {
+                            GameObject face = row[i].transform.GetChild(j).gameObject;
+                            face.GetComponent<Renderer>().material = newMat;
+                        }
+                    }
+                }
+                else
+                {
+                    Vector3 direction = hit.point - m_resultRayCast.positionMouse;
+                    direction.Normalize();
+                    
+                }
+            }
+        }
+        else
+        {
+            m_resultRayCast.isDefinited = false;
+        }
         
+    }
+
+    List<GameObject> GetColumn(int Index, Vector3 normal)
+    {
+        List<GameObject> column = new List<GameObject>();
+
+        if (normal == Vector3.right || normal == Vector3.left)
+        {
+            for (int i = Index / sizeRubiksCube * sizeRubiksCube; i < m_cubes.Count; i++)
+            {
+                if (i >= Index / sizeRubiksCube * sizeRubiksCube + sizeRubiksCube)
+                    return column;
+                column.Add(m_cubes[i]);
+            }
+            return column;
+        }
+        
+        for (int i = Index % m_heigth; i < m_cubes.Count; i += m_heigth)
+            column.Add(m_cubes[i]);
+
+        return column;
+    }
+
+    List<GameObject> GetRow(int Index, Vector3 normal)
+    {
+        List<GameObject> row = new List<GameObject>();
+
+        if (normal == Vector3.up || normal == Vector3.down)
+        {
+            for (int i = Index / sizeRubiksCube * sizeRubiksCube; i < m_cubes.Count; i++)
+            {
+                if (i >= Index / sizeRubiksCube * sizeRubiksCube + sizeRubiksCube)
+                    return row;
+                row.Add(m_cubes[i]);
+            }
+            return row;
+        }
+
+        int inccrement = 0;
+        int indexFirstFace = Index < sizeRubiksCube ? Index / m_width * m_heigth: (Index % (sizeRubiksCube) - 1) / m_width * m_width;
+        for (int i = indexFirstFace; i < m_cubes.Count; i++)
+        {
+            if (i % m_width == 0 && i != indexFirstFace && i + sizeRubiksCube - m_width < m_cubes.Count)
+                i += sizeRubiksCube - m_width;
+
+            inccrement += 1;
+            
+            if (inccrement > m_depth * m_width)
+                return row;
+            
+            row.Add(m_cubes[i]);
+        }
+        return row;
     }
 }
