0d6a12dd52e0dc3e667cc80b781ada2f7630ce6c
Jonathan <j.six@student.isartdigital.com>
2020-12-07 23:25:36 +0100

    Add UI

---
+++ Assets/Scripts/TextSliderLink.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextSliderLink : MonoBehaviour
+{
+    private Text m_text;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_text = GetComponent<Text>();
+    }
+
+    public void SetTextWithFloat (float sliderValue)
+    {
+        m_text.text = sliderValue.ToString();
+    }
+}
