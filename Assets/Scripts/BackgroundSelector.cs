c1fdc40ef7d23a0520d8fd18323ba24d3dcb3fd9
Jonathan <j.six@student.isartdigital.com>
2020-12-12 15:47:54 +0100

    Add back ground and background selection

---
+++ Assets/Scripts/BackgroundSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class BackgroundSelector : MonoBehaviour
+{
+    [SerializeField] private Sprite[] m_backgroundContainer;
+    private Image m_currentImage;
+    private int indexCurrentBackground = 0;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_currentImage = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        m_currentImage.sprite = m_backgroundContainer[0];
+    }
+
+    public void ChangeBackground()
+    {
+        indexCurrentBackground++;
+        if (indexCurrentBackground > m_backgroundContainer.Length - 1)
+            indexCurrentBackground = 0;
+        
+        m_currentImage.sprite = m_backgroundContainer[indexCurrentBackground];
+    }
+}
